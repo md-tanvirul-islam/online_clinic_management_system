39652f812f9f9a33f4b1f793cb0de91f99a5ce44
Md Sohel Rana <info.kmsohel@gmail.com>
2021-01-20 13:07:23 +0600

    testtype seeder added on database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,6 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminUserSeeder::class,
             DepartmentSeeder::class,
+            TestTypeSeeder::class,
         ]);
     }
 }
